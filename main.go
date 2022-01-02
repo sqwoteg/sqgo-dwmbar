31287820082b3bb9712575fd47d7922dd383a53a
sqwoteg <sqwoteg@yahoo.com>
2022-01-02 14:13:23 +0200

    replaced xsetroot with xproto.ChangeProperty

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"os/exec"
+	"log"
 	"strings"
 	"time"
 
 	"./model"
+	"github.com/BurntSushi/xgb"
+	"github.com/BurntSushi/xgb/xproto"
 )
 
 var barStatus = make(map[string]model.BarModuleData)
 
-func setBarStatus(elements map[string]model.BarModuleData) {
+func setBarStatus(elements map[string]model.BarModuleData, x *xgb.Conn, xRoot xproto.Window) {
 	var parts []string
 	for _, module := range elements {
 		if strings.TrimSpace(module.Output) != "" {
@@ -18,19 +20,27 @@ func setBarStatus(elements map[string]model.BarModuleData) {
 		}
 	}
 
-	status := strings.Join(parts, SEPARATOR) + AFTER_STATUS_TEXT
-	exec.Command("xsetroot", "-name", status).Run()
+	status := []byte(strings.Join(parts, SEPARATOR) + AFTER_STATUS_TEXT)
+	xproto.ChangeProperty(x, xproto.PropModeReplace, xRoot, xproto.AtomWmName, xproto.AtomString, 8, uint32(len(status)), status)
 }
 
-func setStatusWorker() {
+func setStatusWorker(x *xgb.Conn, xRoot xproto.Window) {
 	for {
-		setBarStatus(barStatus)
+		setBarStatus(barStatus, x, xRoot)
 		time.Sleep(time.Second * 5)
 	}
 }
 
 func main() {
-	go setStatusWorker() // periodically update status
+	// establish connection to X server
+	x, err := xgb.NewConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer x.Close()
+	xRoot := xproto.Setup(x).DefaultScreen(x).Root
+
+	go setStatusWorker(x, xRoot) // periodically update status
 
 	updatesChannel := make(chan model.BarModuleData)
 
@@ -46,7 +56,7 @@ func main() {
 		barStatus[m.Name] = model.BarModuleData{Name: m.Name, Output: m.Output}
 
 		if m.ImmediatelyUpdate {
-			setBarStatus(barStatus)
+			setBarStatus(barStatus, x, xRoot)
 		}
 	}
 }
